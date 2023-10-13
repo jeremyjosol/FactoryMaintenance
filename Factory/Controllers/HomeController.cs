1b9e315e4186b6eb2da39faf3ea84ca73c189248
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-13 13:48:29 -0700

    :hammer_and_wrench:

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,20 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Factory.Models;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
-    [HttpGet("/")]
+    private readonly FactoryContext _db;
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
     public ActionResult Index()
     {
-      return View();
+
     }
   }
 }
---
+++ Factory/Models/Factory.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+    public class Factory
+    {
+        public List<Engineer> Engineers { get; set; }
+        public List<Machine> Machines { get; set; }
+    }
+}
